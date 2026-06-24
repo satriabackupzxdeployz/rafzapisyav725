9a21d6f4685c3b53f64edfae4aaf53cf8a42fc17
satriabackupzxdeployz <satriabackupz@gmail.com>
2026-06-24 15:41:51 +0700

    Add app/d/threads/route.js

---
+++ app/d/threads/route.js
@@ -0,0 +1,8 @@
+import { threadsDownload } from "@/lib/scrapers/threads";
+import { ok, fail, badRequest, getParam } from "@/lib/response";
+export const dynamic = "force-dynamic";
+export async function GET(req) {
+  const url = getParam(req,"url");
+  if (!url) return badRequest("Parameter 'url' wajib diisi");
+  try { return ok(await threadsDownload(url)); } catch(e){ return fail(e.message); }
+}
