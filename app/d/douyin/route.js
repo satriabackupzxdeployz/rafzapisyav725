17f0f627bad5323ea3a696c60566b37583e7eae6
satriabackupzxdeployz <satriabackupz@gmail.com>
2026-06-24 15:41:34 +0700

    Add app/d/douyin/route.js

---
+++ app/d/douyin/route.js
@@ -0,0 +1,8 @@
+import { douyinDownload } from "@/lib/scrapers/douyin";
+import { ok, fail, badRequest, getParam } from "@/lib/response";
+export const dynamic = "force-dynamic";
+export async function GET(req) {
+  const url = getParam(req,"url");
+  if (!url) return badRequest("Parameter 'url' wajib diisi");
+  try { return ok(await douyinDownload(url)); } catch(e){ return fail(e.message); }
+}
